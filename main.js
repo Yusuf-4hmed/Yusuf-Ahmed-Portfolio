346a9c1f7ed6fe4bad3de36eaa372e9209d63763
Yusuf Ahmed <108082685+Yusuf-4hmed@users.noreply.github.com>
2024-08-25 13:09:55 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function toggleDetails() {
+    const details = document.getElementById('experience-details');
+    // Toggle the 'open' attribute of the details element
+    if (details.hasAttribute('open')) {
+    details.removeAttribute('open');
+    } else {
+    details.setAttribute('open', 'open');
+    }
+}
+function toggleDetailsOne() {
+    const details = document.getElementById('experience-details-one');
+    // Toggle the 'open' attribute of the details element
+    if (details.hasAttribute('open')) {
+    details.removeAttribute('open');
+    } else {
+    details.setAttribute('open', 'open');
+    }
+}
+function toggleDetailsTwo() {
+    const details = document.getElementById('experience-details-two');
+    // Toggle the 'open' attribute of the details element
+    if (details.hasAttribute('open')) {
+    details.removeAttribute('open');
+    } else {
+    details.setAttribute('open', 'open');
+    }
+}
