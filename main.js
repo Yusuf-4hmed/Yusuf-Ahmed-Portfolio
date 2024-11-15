73f51330333335e6c4e2632173353ab39557f67a
Yusuf Ahmed <yusufahmed0625@gmail.com>
2024-11-15 19:26:04 +0000

    update v2.4
    -Updated projects section with CSS animations that allow easy recognition of what projects are live.
    -Media query bug fixed where the project grid container was ugly in smaller devices.

--- main.js
+++ main.js
@@ -1,27 +1,18 @@
-function toggleDetails() {
-    const details = document.getElementById('experience-details');
-    // Toggle the 'open' attribute of the details element
-    if (details.hasAttribute('open')) {
-    details.removeAttribute('open');
+
+const meHeader = document.getElementById("me-header-h1");
+
+meHeader.addEventListener("click", () => {
+    if (meHeader.textContent === "Yusuf Ahmed"){
+        meHeader.textContent = "Yusuf Ahmed 😁";    
     } else {
-    details.setAttribute('open', 'open');
+        meHeader.textContent = "Yusuf Ahmed"
     }
-}
-function toggleDetailsOne() {
-    const details = document.getElementById('experience-details-one');
-    // Toggle the 'open' attribute of the details element
-    if (details.hasAttribute('open')) {
-    details.removeAttribute('open');
-    } else {
-    details.setAttribute('open', 'open');
-    }
-}
-function toggleDetailsTwo() {
-    const details = document.getElementById('experience-details-two');
-    // Toggle the 'open' attribute of the details element
-    if (details.hasAttribute('open')) {
-    details.removeAttribute('open');
-    } else {
-    details.setAttribute('open', 'open');
-    }
-}
+    
+});
+
+const unavailableButton = document.getElementById('unavailable');
+
+unavailableButton.addEventListener("click", ()=>{
+    unavailableButton.textContent = "CAN U READ?"
+})
+
